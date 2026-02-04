54c264077bc20e425468dad7cae2569619f88823
unknown <iandang312@gmail.com>
2026-02-04 16:45:58 -0600

    base pages

---
+++ frontend/app/home/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+import Orb from "@/components/Orb";
+
+export default function HomePage() {
+    return (
+        <div style={{ width: '1080px', height: '1080px', position: 'relative' }}>
+            <Orb
+                hue={0}
+                hoverIntensity={0}
+                rotateOnHover
+                forceHoverState={false}
+            />
+        </div>
+    );
+}
