99ec4a2f0ce0b55e7061ae5b21e5605cefaa006e
unknown <iandang312@gmail.com>
2026-02-05 00:48:31 -0600

    ci pipeline setup

--- frontend/app/home/page.tsx
+++ frontend/app/home/page.tsx
@@ -1,15 +1,10 @@
-"use client"
-import Orb from "@/components/Orb";
+'use client';
+import Orb from '@/components/Orb';
 
 export default function HomePage() {
-    return (
-        <div style={{ width: '1080px', height: '1080px', position: 'relative' }}>
-            <Orb
-                hue={0}
-                hoverIntensity={0}
-                rotateOnHover
-                forceHoverState={false}
-            />
-        </div>
-    );
+  return (
+    <div style={{ width: '1080px', height: '1080px', position: 'relative' }}>
+      <Orb hue={0} hoverIntensity={0} rotateOnHover forceHoverState={false} />
+    </div>
+  );
 }
